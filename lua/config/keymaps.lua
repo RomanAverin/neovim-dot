747b32f0e150af35307a3903529af704bad582f2
Roman Averin <mail@romanaverin.com>
2024-06-03 13:19:13 +0300

    Init comfiguration

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,19 @@
+-- Set indentation options
+vim.opt.tabstop =  4
+vim.opt.softtabstop =  4
+vim.opt.shiftwidth =  4
+vim.opt.expandtab = true
+-- Uncomment the lines below if you want to enable autoindent and smartindent
+-- vim.opt.autoindent = true
+-- vim.opt.smartindent = true
+
+-- Define normal mode mappings
+-- Make sure to use 'n' for normal mode mappings
+-- Autoindent after pasting
+vim.api.nvim_set_keymap('n', 'p', 'p=`]', {noremap = true})
+vim.api.nvim_set_keymap('n', 'P', 'P=`]', {noremap = true})
+
+-- Current line chages
+vim.wo.relativenumber = true
+vim.wo.number = true
+vim.opt.numberwidth = 1
