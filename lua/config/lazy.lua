ac195f62e68c7adcbe179b55aa51fb3ae3477b05
Roman Averin <mail@romanaverin.com>
2024-09-10 21:27:18 +0300

    Add new plugins

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -25,6 +25,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.go" },
+    { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.coding.mini-comment" },
     -- import/override with your plugins
     { import = "plugins" },
   },
