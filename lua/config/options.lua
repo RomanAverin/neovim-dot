1d2fc3b3d5762b829c0636cc7d4dff82892ac264
Roman Averin <mail@romanaverin.com>
2024-09-17 23:23:08 +0300

    Fix tab options in options.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,10 +4,8 @@
 --
 -- Set indentation options
 vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
--- vim.opt.softtabstop =  4
+-- vim.opt.softtabstop = 4
+-- vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
