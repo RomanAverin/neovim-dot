13ac63f430ddd5e6b89bd4a6383ce1a67f4190c5
Roman Averin <mail@romanaverin.com>
2024-11-19 19:44:35 +0300

    Change dashboard on the new from snack.nvim

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,68 +1,21 @@
 return {
-  "nvimdev/dashboard-nvim",
+  "folke/snacks.nvim",
+  priority = 1000,
   lazy = false,
-  opts = function()
-    local table_logo = {
-      [[                                                                     ]],
-      [[       ███████████           █████      ██                     ]],
-      [[      ███████████             █████                             ]],
-      [[      ████████████████ ███████████ ███   ███████     ]],
-      [[     ████████████████ ████████████ █████ ██████████████   ]],
-      [[    ██████████████    █████████████ █████ █████ ████ █████   ]],
-      [[  ██████████████████████████████████ █████ █████ ████ █████  ]],
-      [[ ██████  ███ █████████████████ ████ █████ █████ ████ ██████ ]],
-    }
-    local function joinStrings(arr, delimiter)
-      return table.concat(arr, delimiter)
-    end
-    local logo = joinStrings(table_logo, "\n")
-
-    logo = string.rep("\n", 8) .. logo .. "\n\n"
-
-    local opts = {
-      theme = "hyper",
-      shortcut_type = "number",
-      config = {
-        header = vim.split(logo, "\n"),
-        shortcut = {
-          { desc = "󰊳 Update", group = "@property", action = "Lazy update", key = "u" },
-          {
-            icon = " ",
-            icon_hl = "@variable",
-            desc = "Files",
-            group = "Label",
-            action = "Telescope find_files",
-            key = "f",
-          },
-          {
-            desc = "  Keymaps",
-            group = "DiagnosticHint",
-            action = "Telescope keymaps",
-            key = "m",
-          },
-          {
-            desc = " dotfiles",
-            group = "Number",
-            action = "Telescope dotfiles",
-            key = "d",
-          },
-        },
+  opts = {
+    dashboard = {
+      sections = {
+        { section = "header" },
+        { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+        { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+        { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+        { section = "startup" },
       },
-    }
-
-    -- open dashboard after closing lazy
-    if vim.o.filetype == "lazy" then
-      vim.api.nvim_create_autocmd("WinClosed", {
-        pattern = tostring(vim.api.nvim_get_current_win()),
-        once = true,
-        callback = function()
-          vim.schedule(function()
-            vim.api.nvim_exec_autocmds("UIEnter", { group = "dashboard" })
-          end)
-        end,
-      })
-    end
-
-    return opts
-  end,
+      bigfile = { enabled = true },
+      notifier = { enabled = true },
+      quickfile = { enabled = true },
+      statuscolumn = { enabled = true },
+      words = { enabled = true },
+    },
+  },
 }
