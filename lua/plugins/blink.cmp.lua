d9783eaaee4457664f0f4d7d374e59672f48931d
Roman Averin <mail@romanaverin.com>
2024-12-19 21:46:15 +0300

    Some fixes for the blink.cmp

--- lua/plugins/blink.cmp.lua
+++ lua/plugins/blink.cmp.lua
@@ -1,9 +1,17 @@
 return {
   "saghen/blink.cmp",
   opts = {
-
+    ignature = { enabled = true },
     appearance = {
       use_nvim_cmp_as_default = true,
+    },
+    completion = {
+      trigger = {
+        show_on_insert_on_trigger_character = false,
+      },
+      ghost_text = {
+        enabled = false,
+      },
     },
   },
 }
