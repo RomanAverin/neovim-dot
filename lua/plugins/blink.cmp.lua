e156bb1cd34d130985258bbb80ff25725552a47d
Roman Averin <mail@romanaverin.com>
2024-12-26 09:19:46 +0300

    Added some options for the blink.cmp

--- lua/plugins/blink.cmp.lua
+++ lua/plugins/blink.cmp.lua
@@ -1,7 +1,7 @@
 return {
   "saghen/blink.cmp",
   opts = {
-    signature = { enabled = true },
+    signature = { enabled = false },
     appearance = {
       use_nvim_cmp_as_default = false,
     },
@@ -9,8 +9,21 @@ return {
       trigger = {
         show_on_insert_on_trigger_character = false,
       },
+
       ghost_text = {
         enabled = false,
+      },
+      -- Show documentation when selecting a completion item
+      documentation = {
+        auto_show = false,
+        window = {
+          border = "rounded",
+        },
+      },
+      menu = {
+        border = "rounded",
+        draw = { gap = 2 },
+        -- winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine: BlinkCmpMenuSelection,Search:None",
       },
     },
   },
