ae790bf9cfa8bf06747954f9358482a587d3ecc6
Roman Averin <mail@romanaverin.com>
2025-01-15 22:59:52 +0300

    Disabled short hex color highlighting

--- lua/plugins/nvim-highlight-colors.lua
+++ lua/plugins/nvim-highlight-colors.lua
@@ -5,15 +5,9 @@ return {
       ---Render style
       ---@usage 'background'|'foreground'|'virtual'
       render = "virtual",
-
       virtual_symbol = "■",
-
-      ---Set virtual symbol position()
-      ---@usage 'inline'|'eol'|'eow'
-      ---inline mimics VS Code style
-      ---eol stands for `end of column` - Recommended to set `virtual_symbol_suffix = ''` when used.
-      ---eow stands for `end of word` - Recommended to set `virtual_symbol_prefix = ' ' and virtual_symbol_suffix = ''` when used.
       virtual_symbol_position = "inline",
+      enable_short_hex = false,
     })
   end,
 }
