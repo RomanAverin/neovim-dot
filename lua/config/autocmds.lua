b51c0d48a1d7841383c2f68e484187a08d3c3d07
Roman Averin <mail@romanaverin.com>
2025-01-18 13:01:13 +0300

    Moved sonokai override to the autocmds file

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,19 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+--
+-- Must be declared before executing ':colorscheme'.
+local grpid = vim.api.nvim_create_augroup("custom_highlights_sonokai", {})
+vim.api.nvim_create_autocmd("ColorScheme", {
+  group = grpid,
+  pattern = "sonokai",
+  callback = function()
+    local config = vim.fn["sonokai#get_configuration"]()
+    local palette = vim.fn["sonokai#get_palette"](config.style, config.colors_override)
+    local set_hl = vim.fn["sonokai#highlight"]
+
+    set_hl("Visual", palette.none, palette.grey_dim)
+    set_hl("IncSearch", palette.bg0, palette.yellow)
+    set_hl("Search", palette.none, palette.diff_yellow)
+  end,
+})
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -13,22 +13,7 @@ vim.opt.smartindent = true
 -- Cursor shape
 vim.opt.guicursor =
   "n-v-c-sm:block,ci-ve:ver20,r-cr-o:hor20,i:ver20,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
+-- vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20,t:ver25"
 
 -- Enable .editconfig
 vim.g.editorconfig = true
-
--- Must be declared before executing ':colorscheme'.
-local grpid = vim.api.nvim_create_augroup("custom_highlights_sonokai", {})
-vim.api.nvim_create_autocmd("ColorScheme", {
-  group = grpid,
-  pattern = "sonokai",
-  callback = function()
-    local config = vim.fn["sonokai#get_configuration"]()
-    local palette = vim.fn["sonokai#get_palette"](config.style, config.colors_override)
-    local set_hl = vim.fn["sonokai#highlight"]
-
-    set_hl("Visual", palette.none, palette.grey_dim)
-    set_hl("IncSearch", palette.bg0, palette.yellow)
-    set_hl("Search", palette.none, palette.diff_yellow)
-  end,
-})
