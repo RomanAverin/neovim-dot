85fb40cb5e03193793d5a96d05d39e93115d1ba0
Roman Averin <mail@romanaverin.com>
2025-01-18 13:02:28 +0300

    Disable diagnostics virtual_text for the lsp_lines

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -2,5 +2,9 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     inlay_hints = { enabled = false },
+    -- Disable virtual_text since it's redundant due to lsp_lines.
+    diagnostics = {
+      virtual_text = false,
+    },
   },
 }
