76817af61f4dd612e4bbf9a405fa7109cf069fc4
Roman Averin <mail@romanaverin.com>
2025-02-09 12:33:09 +0300

    Added reload plugins keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,16 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<leader>ur", function()
+  local plugins = require("lazy").plugins()
+  local plugin_names = {}
+  for _, plugin in ipairs(plugins) do
+    table.insert(plugin_names, plugin.name)
+  end
+
+  vim.ui.select(plugin_names, {
+    title = "Reload plugin",
+  }, function(selected)
+    require("lazy").reload({ plugins = { selected } })
+  end)
+end, { desc = "Reload plugin" })
