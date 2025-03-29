e9ad5b5ed7cdc1e545354b76f3c4b19a6910b775
Roman Averin <mail@romanaverin.com>
2025-03-29 09:55:33 +0300

    Enabled inline hints

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,7 +1,7 @@
 return {
   "neovim/nvim-lspconfig",
   opts = {
-    inlay_hints = { enabled = false },
+    inlay_hints = { enabled = true },
     -- Disable virtual_text since it's redundant due to lsp_lines.
     diagnostics = {
       virtual_text = false,
