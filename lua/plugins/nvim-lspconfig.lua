c51cb1dba606a510e62cceac31f6b8bcdd75254f
Roman Averin <mail@romanaverin.com>
2025-07-04 14:16:00 +0300

    Added lsp support for jinja2 templales

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -2,5 +2,10 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     inlay_hints = { enabled = true },
+    servers = {
+      jinja_lsp = {
+        filetypes = { "jinja", "html", "j2" },
+      },
+    },
   },
 }
