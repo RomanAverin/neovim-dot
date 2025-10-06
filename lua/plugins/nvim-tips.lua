0c4cc916edfda7874f2ddee167819b40f2c5e3d3
Roman Averin <mail@romanaverin.com>
2025-10-06 15:08:14 +0300

    Added neovim-tips plugin

---
+++ lua/plugins/nvim-tips.lua
@@ -0,0 +1,13 @@
+return {
+  "saxon1964/neovim-tips",
+  version = "*", -- Only update on tagged releases
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "MeanderingProgrammer/render-markdown.nvim",
+  },
+  opts = {
+    -- OPTIONAL: Daily tip mode (default: 1)
+    -- 0 = off, 1 = once per day, 2 = every startup
+    daily_tip = 1,
+  },
+}
