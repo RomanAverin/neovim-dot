dc2d8fc8874ae6ccd61d83aa4b8a01e62634f17a
Roman Averin <mail@romanaverin.com>
2025-12-20 18:44:56 +0300

    feat(fzf): rewrite color of grep for fzf-lua and use fzf colors in NeoVim

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -6,5 +6,10 @@ return {
         layout = "vertical",
       },
     },
+    fzf_colors = true,
+    grep = {
+      rg_opts = "--column --line-number --no-heading --color=always --smart-case --max-columns=4096 "
+        .. "--colors=match:fg:214 --colors=match:style:bold -e",
+    },
   },
 }
