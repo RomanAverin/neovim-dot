170385c86907c0bb408828418c291ec129d34deb
Roman Averin <mail@romanaverin.com>
2026-02-03 10:57:16 +0300

    fix: reset cursor to beam on exit

--- init.lua
+++ init.lua
@@ -1,2 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Reset cursor to beam on exit
+vim.api.nvim_create_autocmd("VimLeave", {
+  callback = function()
+    vim.opt.guicursor = "a:ver25-blinkon1"
+    io.write("\27[5 q")
+  end,
+})
