522c8642d5b322aad965fee7b0c43188eccf9fc0
Roman Averin <mail@romanaverin.com>
2026-02-05 20:02:43 +0300

    fix(keymaps): free Alt+h/j/k/l for tmux

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -21,3 +21,20 @@ vim.keymap.set("n", "<leader>uv", function()
 
   vim.diagnostic.config(config)
 end, { desc = "Toggle diagnostic virtual_lines" })
+
+-- Free Alt+h/j/k/l for tmux
+local del = vim.keymap.del
+-- A-j/A-k (move lines) set by LazyVim
+pcall(del, "n", "<A-j>")
+pcall(del, "n", "<A-k>")
+pcall(del, "i", "<A-j>")
+pcall(del, "i", "<A-k>")
+pcall(del, "v", "<A-j>")
+pcall(del, "v", "<A-k>")
+-- A-h/A-l not set by LazyVim globally, but delete just in case
+pcall(del, "n", "<A-h>")
+pcall(del, "n", "<A-l>")
+pcall(del, "i", "<A-h>")
+pcall(del, "i", "<A-l>")
+pcall(del, "v", "<A-h>")
+pcall(del, "v", "<A-l>")
